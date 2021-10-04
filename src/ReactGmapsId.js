63d5d3bf8497af5f5e59d1f1ec42a38f058c0853
Ugi Ispoyo Widodo <ugi.widodo@dataon.com>
2021-10-04 08:53:23 +0700

    update message error

--- src/ReactGmapsId.js
+++ src/ReactGmapsId.js
@@ -112,7 +112,8 @@ export default function ReactMapsId({
           return results;
         })
         .catch((e) => {
-          alert("Geocode was not successful for the following reason: " + e);
+          // alert("Geocode was not successful for the following reason: " + e);
+          console.log("Geocode was not successful for the following reason: ", e);
         });
     } else {
       clear();
@@ -134,7 +135,8 @@ export default function ReactMapsId({
           return results;
         })
         .catch((e) => {
-          alert("Geocode was not successful for the following reason: " + e);
+          // alert("Geocode was not successful for the following reason: " + e);
+          console.log("Geocode was not successful for the following reason: ", e);
         });
     }
   };
